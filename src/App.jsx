d8a2a07affe7f4eef13a82c833c2f76fbc773c6a
Charles Silva <charlesipn@gmail.com>
2024-06-18 21:13:22 -0600

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  // lista de todos
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  function addTodo() {
+    setTodos([...todos, text]);
+  }
+
+  function removeTodo(indexToRemove) {
+    // todos.splice(indexToRemove, 1);
+    // setTodos([...todos]);
+
+    const newTodos = todos.filter((todo, idx) => idx !== indexToRemove);
+    setTodos(newTodos);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    addTodo();
+    setText("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full min-h-screen flex flex-col">
+      <form
+        className="flex flex-row gap-2 justify-center p-5"
+        onSubmit={onSubmit}
+      >
+        <input
+          name="todo"
+          type="text"
+          className="p-2 rounded text-black w-full max-w-screen-sm"
+          placeholder="Ingresa una tarea"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          required
+        />
+        <button className="bg-white  text-black px-3 rounded">+ Agregar</button>
+      </form>
+
+      <div className="max-w-screen-sm w-full mx-auto p-4 flex flex-col gap-1">
+        {todos.length === 0 && (
+          <p className="text-white/50">No tienes tareas pendientes 🤷‍♂️</p>
+        )}
+        {todos.length > 0 &&
+          todos.map((todo, idx) => {
+            return (
+              <div
+                key={`todo-${idx}`}
+                className="bg-white/10 rounded p-4 flex flex-row justify-between select-none"
+              >
+                <span>{todo}</span>
+                <span
+                  className="text-red-500 cursor-pointer hover:bg-red-500 hover:text-white rounded-full p-1 size-5 text-center items-center flex"
+                  onClick={() => removeTodo(idx)}
+                >
+                  ⛌
+                </span>
+              </div>
+            );
+          })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
-
-export default App;
